34c6b3b2851bf6743fc5ae3be26edb4292fea039
Hussain Malak <husainmalak21@gmail.com>
2022-10-17 19:38:37 +0530

    Update auto_image_alt.js
    Fixed.Issues

--- Image and title tag/js/auto_image_alt.js
+++ Image and title tag/js/auto_image_alt.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
-  jQuery("img").each(function() { 
+  jQuery('img').each(function() { 
       var img_src = jQuery(this).attr('src');
       if (typeof img_src !== typeof undefined && img_src !== false) {
       var img_alt = jQuery(this).attr('alt');
-      var str = img_src
+      var str = img_src;
       var pieces = str.split('/');
       var imgName = pieces[pieces.length-1];
       var imgnameArray = imgName.split('.');
